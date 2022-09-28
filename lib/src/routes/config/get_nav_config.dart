7099b5b0a4490bb76bfc712dd4396d4874f08a5a
the-best-is-best <michelle.raouf@outlook.com>
2022-09-28 18:49:14 +0200

    remove code not used

--- lib/src/routes/config/get_nav_config.dart
+++ lib/src/routes/config/get_nav_config.dart
@@ -1,24 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:mit_x/mit_x.dart';
-import 'package:mit_x/src/routes/mit_route.dart';
 
-// class GetRouterState extends GetxController {
-//   GetRouterState({required this.currentTreeBranch});
-//   final List<MitXPage> currentTreeBranch;
-//   MitXPage? get currentPage => currentTreeBranch.last;
-
-//   static GetNavConfig? fromRoute(String route) {
-//     final res = Get.routeTree.matchRoute(route);
-//     if (res.treeBranch.isEmpty) return null;
-//     return GetNavConfig(
-//       currentTreeBranch: res.treeBranch,
-//       location: route,
-//       state: null,
-//     );
-//   }
-// }
-
-/// This config enables us to navigate directly to a sub-url
 class GetNavConfig extends RouteInformation {
   final List<MitXPage> currentTreeBranch;
   MitXPage? get currentPage => currentTreeBranch.last;
